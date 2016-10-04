e9291431a49878eed20fd816a686d94eb05a503c
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-10-03 21:33:28 -0500

    completed Localization program

--- 1 - Localization/localization.py
+++ 1 - Localization/localization.py
@@ -49,8 +49,33 @@ def localize(colors,measurements,motions,sensor_right,p_move):
     p = [[pinit for row in range(len(colors[0]))] for col in range(len(colors))]
     
     # >>> Insert your code here <<<
-    
+    for i in range(len(measurements)):
+        p = move(p, motions[i], p_move, colors)
+        p = sense(p, measurements[i], sensor_right, colors)
     return p
+
+def sense(p, Z, pHit, colors):
+    pMiss = 1. - pHit
+    q = [[0 for row in range(len(colors[0]))] for col in range(len(colors))]
+    for x in range(len(p[0])):
+        for y in range(len(p)):
+            hit = (Z == colors[y][x]) 
+            q[y][x] = (p[y][x] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(map(sum,q))
+    for x in range(len(q[0])):
+        for y in range(len(q)):
+            q[y][x] = q[y][x] / s
+    return q
+
+def move(p, U, p_move, colors):
+    p_stay = 1. - p_move
+    q = [[0 for row in range(len(colors[0]))] for col in range(len(colors))]
+    for x in range(len(q[0])):
+        for y in range(len(q)):
+            s = p_move * p[(y-U[0]) % len(p)][(x-U[1]) % len(p[0])]
+            s += p_stay * p[y][x]
+            q[y][x] = s
+    return q
 
 def show(p):
     rows = ['[' + ','.join(map(lambda x: '{0:.5f}'.format(x),r)) + ']' for r in p]
