fd1f19bd81b9b53a1c6f145f56fb2e9f1e78df7f
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-10-06 21:50:18 -0500

    Kalman Filters final project complete

--- 2 - Kalman Filters/matrices2.py
+++ 2 - Kalman Filters/matrices2.py
@@ -182,11 +182,18 @@ u = matrix([[0.], [0.], [0.], [0.]]) # external motion
 #### DO NOT MODIFY ANYTHING ABOVE HERE ####
 #### fill this in, remember to use the matrix() function!: ####
 
-P =  # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
-F =  # next state function: generalize the 2d version to 4d
-H =  # measurement function: reflect the fact that we observe x and y but not the two velocities
-R =  # measurement uncertainty: use 2x2 matrix with 0.1 as main diagonal
-I =  # 4d identity matrix
+P =  matrix([[0.,0.,0.,0.],[0.,0.,0.,0.],[0.,0.,1000.,0.],[0.,0.,0.,1000.]])
+# initial uncertainty: 0 for positions x and y, 1000 for the two velocities
+F =  matrix([[1.,0.,.1,0.],[0.,1.,0.,.1],[0.,0.,1.,0.],[0.,0.,0.,1.]])
+# next state function: generalize the 2d version to 4d
+H =  matrix([[1.,0.,0.,0.],[0.,1.,0.,0.]])
+# measurement function: reflect the fact that we observe x and y but not the two velocities
+R =  matrix([[.1,0],[0,.1]])
+# measurement uncertainty: use 2x2 matrix with 0.1 as main diagonal
+I =  matrix([[]])
+I.identity(4)
+# 4d identity matrix
+
 
 ###### DO NOT MODIFY ANYTHING HERE #######
 
