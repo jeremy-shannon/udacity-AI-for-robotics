1da0d2fea7313c251117bce0fd6409ad55bd3022
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-10-13 18:43:13 -0500

    ...and this is my solution to First Search

--- 4 - Search/firstSearch.py
+++ 4 - Search/firstSearch.py
@@ -15,11 +15,11 @@
 #   0 = Navigable space
 #   1 = Occupied space
 
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0]]
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
@@ -35,5 +35,28 @@ def search(grid,init,goal,cost):
     # ----------------------------------------
     # insert code here
     # ----------------------------------------
-
+    take = [0,init[0],init[1]]
+    print "take:", take
+    open = []
+    path = "fail"
+    while True:
+        if take[1] == goal[0] and take[2] == goal[1]:
+            print "success!"
+            path = take
+            break
+        for dir in delta:
+            cell = [take[0]+cost,take[1]+dir[0],take[2]+dir[1]]
+            if cell[1] > -1 and cell[1] < len(grid) and cell[2] > -1 and\
+            cell[2] < len(grid[0]) and grid[cell[1]][cell[2]] == 0:
+                open.append(cell)
+        grid[take[1]][take[2]] = 2
+        if len(open) == 0:
+            break
+        open.sort()
+        print "open:", open
+        take = open[0]
+        print "take:", take
+        open.pop(0)
     return path
+
+print search(grid,init,goal,cost)
