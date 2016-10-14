cc7656dda6447647f04da877bd20bd23e171b83d
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-10-14 16:03:25 -0500

    Implement A* exercise solution

--- 4 - Search/implementAStar.py
+++ 4 - Search/implementAStar.py
@@ -48,8 +48,9 @@ def search(grid,init,goal,cost,heuristic):
     x = init[0]
     y = init[1]
     g = 0
+    f = g + heuristic[x][y]
 
-    open = [[g, x, y]]
+    open = [[f, g, x, y]]
 
     found = False  # flag that is set when search is complete
     resign = False # flag set if we can't find expand
@@ -63,9 +64,10 @@ def search(grid,init,goal,cost,heuristic):
             open.sort()
             open.reverse()
             next = open.pop()
-            x = next[1]
-            y = next[2]
-            g = next[0]
+            x = next[2]
+            y = next[3]
+            g = next[1]
+            f = next[0]
             expand[x][y] = count
             count += 1
             
@@ -78,10 +80,11 @@ def search(grid,init,goal,cost,heuristic):
                     if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
                             g2 = g + cost
-                            open.append([g2, x2, y2])
+                            f2 = g2 + heuristic[x2][y2]
+                            open.append([f2, g2, x2, y2])
                             closed[x2][y2] = 1
 
     return expand
-
+    
 for line in search(grid, init,goal,cost, heuristic):
     print line 
