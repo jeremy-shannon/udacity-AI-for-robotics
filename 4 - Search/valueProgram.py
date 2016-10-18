72015dd5f82947da251b43adbbb0679cf68dfc00
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-10-17 21:22:15 -0500

    Value Program exercise solution

--- 4 - Search/valueProgram.py
+++ 4 - Search/valueProgram.py
@@ -28,7 +28,30 @@ def compute_value(grid,goal,cost):
     # ----------------------------------------
     # insert code below
     # ----------------------------------------
-    
+    value = [[99 for x in range(len(grid[0]))] for y in range(len(grid))]
     # make sure your function returns a grid of values as 
     # demonstrated in the previous video.
-    return value 
+    value[goal[0]][goal[1]] = 0
+    openList = []
+    openList.append([0, goal[0],goal[1]])
+    while len(openList) != 0:
+        openList.sort()
+        currentCell = openList.pop(0)
+        for i in range(len(delta)):
+            targetX = currentCell[2] - delta[i][1]
+            targetY = currentCell[1] - delta[i][0]
+            if inGrid(grid, targetX, targetY):
+                if grid[targetY][targetX] == 0 and value[targetY][targetX] == 99:
+                    openList.append([currentCell[0]+cost, targetY, targetX])
+                    value[targetY][targetX] = currentCell[0] + cost
+    return value
+    
+    
+def inGrid(grid, x, y):
+    if x >= 0 and x < len(grid[0]) and y >= 0 and y < len(grid):
+        return True
+    else:
+        return False
+
+for line in compute_value(grid, goal, cost):
+    print line
