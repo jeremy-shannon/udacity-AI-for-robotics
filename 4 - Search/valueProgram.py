7cd9e3478624de4157423264704f59530f4b8d7b
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-10-17 21:21:21 -0500

    Value Program exercise starter code

---
+++ 4 - Search/valueProgram.py
@@ -0,0 +1,34 @@
+# ----------
+# User Instructions:
+# 
+# Create a function compute_value which returns
+# a grid of values. The value of a cell is the minimum
+# number of moves required to get from the cell to the goal. 
+#
+# If a cell is a wall or it is impossible to reach the goal from a cell,
+# assign that cell a value of 99.
+# ----------
+
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1 # the cost associated with moving from a cell to an adjacent one
+
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def compute_value(grid,goal,cost):
+    # ----------------------------------------
+    # insert code below
+    # ----------------------------------------
+    
+    # make sure your function returns a grid of values as 
+    # demonstrated in the previous video.
+    return value 
