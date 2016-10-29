c046782c9a9151526a88326054eb980e60a63200
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-10-29 09:17:35 -0500

    Online SLAM solution

--- 6 - SLAM/onlineSLAM.py
+++ 6 - SLAM/onlineSLAM.py
@@ -566,6 +566,71 @@ def online_slam(data, N, num_landmarks, motion_noise, measurement_noise):
     # Enter your code here!
     #
     #
+        # Set the dimension of the filter
+    dim = 2 * (1 + num_landmarks) 
+
+    # make the constraint information matrix and vector
+    Omega = matrix()
+    Omega.zero(dim, dim)
+    Omega.value[0][0] = 1.0
+    Omega.value[1][1] = 1.0
+
+    Xi = matrix()
+    Xi.zero(dim, 1)
+    Xi.value[0][0] = world_size / 2.0
+    Xi.value[1][0] = world_size / 2.0
+
+    # process the data
+    
+    for k in range(len(data)):
+
+        # n is the index of the robot pose in the matrix/vector
+        # n = k * 2    #.... do not need
+    
+        measurement = data[k][0]
+        motion      = data[k][1]
+    
+        # integrate the measurements
+        for i in range(len(measurement)):
+    
+            # m is the index of the landmark coordinate in the matrix/vector
+            m = 2 * (1 + measurement[i][0])
+    
+            # update the information maxtrix/vector based on the measurement
+            for b in range(2):
+                Omega.value[b][b]     +=  1.0 / measurement_noise
+                Omega.value[m+b][m+b] +=  1.0 / measurement_noise
+                Omega.value[b][m+b]   += -1.0 / measurement_noise
+                Omega.value[m+b][b]   += -1.0 / measurement_noise
+                Xi.value[b][0]        += -measurement[i][1+b] / measurement_noise
+                Xi.value[m+b][0]      +=  measurement[i][1+b] / measurement_noise
+
+        # add zero rows for the new position
+        Omega = Omega.expand(dim+2, dim+2, [0,1] + range(4, dim+2), [0,1] + range(4, dim+2))
+        Xi = Xi.expand(dim+2, 1, [0,1] + range(4, dim+2), [0])
+
+        # update the information maxtrix/vector based on the robot motion
+        for b in range(4):
+            Omega.value[b][b] +=  1.0 / motion_noise
+        for b in range(2):
+            Omega.value[b  ][b+2] += -1.0 / motion_noise
+            Omega.value[b+2][b  ] += -1.0 / motion_noise
+            Xi.value[b  ][0]        += -motion[b] / motion_noise
+            Xi.value[b+2][0]        +=  motion[b] / motion_noise
+
+        matrixB = Omega.take([0,1],[0,1])
+        matrixA = Omega.take([0,1],range(2,dim+2))
+        matrixC = Xi.take([0,1],[0])
+        OmegaPrime = Omega.take(range(2,dim+2),range(2,dim+2))
+        XiPrime = Xi.take(range(2,dim+2), [0])
+
+        Omega = OmegaPrime - matrixA.transpose() * matrixB.inverse() * matrixA
+        Xi = XiPrime - matrixA.transpose() * matrixB.inverse() * matrixC
+
+    # compute best estimate
+    mu = Omega.inverse() * Xi
+
+    # return the result
     return mu, Omega # make sure you return both of these matrices to be marked correct.
 
 # --------------------------------
@@ -685,8 +750,13 @@ answer_omega1      = matrix([[0.36603773584905663, 0.0, -0.169811320754717, 0.0,
                              [-0.1811320754716981, 0.0, -0.4056603773584906, 0.0, -0.360377358490566, 0.0, 1.2339622641509433, 0.0],
                              [0.0, -0.1811320754716981, 0.0, -0.4056603773584906, 0.0, -0.360377358490566, 0.0, 1.2339622641509433]])
 
-#result = online_slam(testdata1, 5, 3, 2.0, 2.0)
-#solution_check(result, answer_mu1, answer_omega1)
+
+
+result = slam(testdata1, 5, 3, 2.0, 2.0)
+result.show()
+result = online_slam(testdata1, 5, 3, 2.0, 2.0)
+result[0].show()
+solution_check(result, answer_mu1, answer_omega1)
 
 
 # -----------
@@ -712,7 +782,20 @@ answer_omega2      = matrix([[0.22871751620895048, 0.0, -0.11351536555795691, 0.
                              [-0.11351536555795691, 0.0, -0.46327947920510265, 0.0, 0.7867205207948973, 0.0],
                              [0.0, -0.11351536555795691, 0.0, -0.46327947920510265, 0.0, 0.7867205207948973]])
 
-#result = online_slam(testdata2, 6, 2, 3.0, 4.0)
-#solution_check(result, answer_mu2, answer_omega2)
+
+result = slam(testdata2, 6, 2, 3.0, 4.0)
+result.show()
+result = online_slam(testdata2, 6, 2, 3.0, 4.0)
+result[0].show()
+solution_check(result, answer_mu2, answer_omega2)
 
 
+# -----------
+# Test Case 3
+# no motion, two steps
+testdata0  = [
+    [[[0, 10., 20.], [1, 30., 40.]], [0., 0.]], 
+    [[[0, 10., 20.], [1, 30., 40.]], [0., 0.]]
+]
+result = online_slam(testdata0, 1, 2, 1.0, 1.0) 
+result[0].show()
